3be9b63e3fcefd5528ff632579a980da0dc5fe8d
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-03-03 05:12:45 +0530

    added some changes

--- server/index.ts
+++ server/index.ts
@@ -58,9 +58,9 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = process.env.PORT || 5000;
+  const port = 5000;
   server.listen({
-    port: Number(port),
+    port,
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
